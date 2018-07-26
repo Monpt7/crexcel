7a92049b917496dd110eafbbcac07365cdcbe185
Holden Omans <holden.omans@gmail.com>
2018-07-26 08:36:05 -0400

    Formatting, more worksheet functions, and a bug fix (#6)
    * added new helper functions to worksheet
    
    * Cleaned up way directory paths were being created. Added temp dirs.
    
    * fix file extension
    
    * added remove of temp dir
    
    * updated to use FileUtils.rm_r
    
    * update crystal v0.25.1
    
    * added require for file_utils

--- spec/workbook_spec.cr
+++ spec/workbook_spec.cr
@@ -4,7 +4,7 @@ require "../src/crexcel/*"
 describe Crexcel do
   name = "test.xlsx"
   workbook = Crexcel::Workbook.new(name)
-  worksheet1 = workbook.add_worksheet()
+  worksheet1 = workbook.add_worksheet
 
   it "creates a workbook with the correct filename" do
     workbook.name.should eq("test.xlsx")
@@ -55,17 +55,28 @@ describe Crexcel do
   end
 
   it "write a string in cell with location" do
-    worksheet1.write(1,1, "hi!")
+    worksheet1.write(1, 1, "hi!")
     Crexcel::SharedString.get_strings.size.should eq(4)
     worksheet1.get_datas.last["type"].should eq("s")
   end
 
   it "write a string as an int in cell with location" do
-    worksheet1.write(1,2, "1337", "int")
+    worksheet1.write(1, 2, "1337", "int")
     worksheet1.get_datas.last["value"].should eq("1337")
     worksheet1.get_datas.last["type"].should eq("n")
   end
 
-  workbook.close
+  it "write an array to row via string" do
+    worksheet1.write_row("Z", [123, 321])
+    worksheet1.get("Z1")[:value].should eq("123")
+    worksheet1.get("Z2")[:value].should eq("321")
+  end
 
+  it "write an array to row via int" do
+    worksheet1.write_row(14, [123, 321])
+    worksheet1.get(0, 14)[:value].should eq("123")
+    worksheet1.get(1, 14)[:value].should eq("321")
+  end
+
+  workbook.close
 end
--- src/helpers/position_helper.cr
+++ src/helpers/position_helper.cr
@@ -1,5 +1,5 @@
 # :nodoc:
 def int_pos_to_char(x : Int32 | Int64, y : Int32 | Int64)
   raise "int_pos_to_char method doesn't allow x > 25 for the moment" if x > 25
-  (x+65).unsafe_chr+(y+1).to_s
+  (x + 65).unsafe_chr + (y + 1).to_s
 end
